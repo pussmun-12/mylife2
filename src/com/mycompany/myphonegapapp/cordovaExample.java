3744afb0d863e0c23a5e89636c3adeec34ff3a39
pussmun-12 <hakan.json@gmail.com>
2014-04-07 10:55:18 +0200

    Update cordovaExample.java

--- src/com/mycompany/myphonegapapp/cordovaExample.java
+++ src/com/mycompany/myphonegapapp/cordovaExample.java
@@ -29,8 +29,10 @@ public class cordovaExample extends DroidGap
     {
         super.onCreate(savedInstanceState);
         // Set by <content src="index.html" /> in config.xml
-        super.loadUrl(Config.getStartUrl());
+        //super.loadUrl(Config.getStartUrl());
         //super.loadUrl("file:///android_asset/www/index.html")
+        super.setIntegerProperty("splashscreen", R.drawable.splash);
+        super.loadUrl(Config.getStartUrl(), 10000);
         
         
     }
